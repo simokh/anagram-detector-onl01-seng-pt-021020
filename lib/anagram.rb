ec248f1ed33ed90ba0f0630265c7d1b8a13fc83c
Simo Khouili <m.simokhouili@gmail.com>
2020-03-08 23:54:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,12 +4,12 @@ class Anagram
    attr_accessor :word
 
   def intialize(word)
-    @word = word
+    @word= word
   end
 
   def match(array_words)
     array_words.select do |word|
      word.split("").sort == @word.split("").sort
-   end 
+   end
    end
 end

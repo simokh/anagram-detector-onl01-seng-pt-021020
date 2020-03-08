cd636d320672333805edb14e900ddd697d064901
Simo Khouili <m.simokhouili@gmail.com>
2020-03-08 23:56:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,25 +7,25 @@ class Anagram
     @word = word
   end
 
-  def match(array_words)
-    array_words.select do |word|
+  def match(array)
+    array.select do |word|
      word.split("").sort == @word.split("").sort
     end
    end
 end
 
 
-# class Anagram
-#
-#   attr_accessor :word
-#
-#   def initialize(word)
-#     @word = word
-#   end
-#
-#   def match(word_array)
-#     word_array.select do |word|
-#       word.split("").sort == @word.split("").sort
-#     end
-#   end
-# end
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end

5da10992c745d9e0d9af888d6d35b9a095f93e35
Simo Khouili <m.simokhouili@gmail.com>
2020-03-08 23:55:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,31 @@
+# class Anagram
+#
+#
+#    attr_accessor :word
+#
+#   def intialize(word)
+#     @word= word
+#   end
+#
+#   def match(array_words)
+#     array_words.select do |word|
+#      word.split("").sort == @word.split("").sort
+#     end
+#    end
+# end
+
+
 class Anagram
 
+  attr_accessor :word
 
-   attr_accessor :word
-
-  def intialize(word)
-    @word= word
+  def initialize(word)
+    @word = word
   end
 
-  def match(array_words)
-    array_words.select do |word|
-     word.split("").sort == @word.split("").sort
-   end
-   end
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
 end

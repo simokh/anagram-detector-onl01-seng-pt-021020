8fa7939845511d38076077d14d2dcacbfbf6c7b4
Simo Khouili <m.simokhouili@gmail.com>
2020-03-09 00:00:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,18 +13,3 @@ class Anagram
       end
    end
 end
-
-class Anagram
-
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-
-  def match(word_array)
-    word_array.select do |word|
-      word.split("").sort == @word.split("").sort
-    end
-  end
-end

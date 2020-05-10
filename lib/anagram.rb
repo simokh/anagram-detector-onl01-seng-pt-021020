230018d710b802ca91ee408fcaad0099dada1afc
simokh <m.simokhouili@gmail.com>
2020-05-10 10:50:53 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,23 @@
-class Anagram
+require 'pry'
+class Anagram 
 
+  attr_accessor :word 
 
-   attr_accessor :word
-
-  def intialize(word)
-    @word = word
+  def initialize(word)
+    @word = word 
   end
 
-  def match(word_array)
-    word_array.select do |word|
-     word.split("").sort == @word.split("").sort
-      end
-   end
+  def match(array)
+    array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end 
+  end 
 end
+
+# binding.pry
+dino = Anagram.new("dino")
+dino.word 
+dino = dino.match(%w(nido doni zach back cake)) #when evoked it will return ["nido", "doni"]
+simo = Anagram.new("simo")
+simo.word
+simo = simo.match(%w(nido doni zach back cake)) #when evoked it will return []
